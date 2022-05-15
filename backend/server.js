58d6aad074bade50d94a38078f5550c76c98f92c
jovellabay <jovellabay@gmail.com>
2022-05-15 12:48:25 +0800

    with reading images

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const fs = require('fs')
-
 const multer = require('multer')
 
+const app = express()
 const upload = multer({ dest: 'images/' })
 
-const app = express()
-
 app.get('/images/:imageName', (req, res) => {
-
-
   const imageName = req.params.imageName
   const readStream = fs.createReadStream(`images/${imageName}`)
   readStream.pipe(res)
+})
+
+app.get('/mama', (req, res) => {
+  fs.readdir("./images", (err, file) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(JSON.stringify(file))
+    }
+  })
 })
 
 app.post('/api/images', upload.single('image'), (req, res) => {
@@ -20,7 +26,6 @@ app.post('/api/images', upload.single('image'), (req, res) => {
   const description = req.body.description
 
   console.log(description, imagePath)
-  res.send({description, imagePath})
 })
 
 app.listen(8080, () => console.log("listening on port 8080"))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,63 @@
-import { useState } from 'react'
-import axios from 'axios'
-
-import './App.css'
-
-
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [file, setFile] = useState();
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState();
 
-  const [file, setFile] = useState()
-  const [description, setDescription] = useState("")
-  const [image, setImage] = useState()
+  const submit = async (event) => {
+    event.preventDefault();
 
-  const submit = async event => {
-    event.preventDefault()
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("description", description);
 
-    const formData = new FormData()
-    formData.append("image", file)
-    formData.append("description", description)
+    const result = await axios.post("/api/images", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    setImage(result);
 
-    const result = await axios.post('/api/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
-    setImage(result)
+    console.log(result);
 
     console.log(image);
-  }
+  };
+
+  const [oo, setOO] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "/mama",
+    })
+      .then((res) => setOO(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="App">
       <form onSubmit={submit}>
         <input
-          filename={file} 
-          onChange={e => setFile(e.target.files[0])} 
-          type="file" 
+          filename={file}
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
           accept="image/*"
         ></input>
         <input
-          onChange={e => setDescription(e.target.value)} 
+          onChange={(e) => setDescription(e.target.value)}
           type="text"
         ></input>
         <button type="submit">Submit</button>
       </form>
 
+      {oo.map((data) => {
+        const qq = "http://localhost:8080/images/" + data;
+        return (
+          <div key={data}>
+            <img height="100px" weight="50px" alt="sdfsdf" src={qq} />
+          </div>
+        );
+      })}
     </div>
   );
 }
